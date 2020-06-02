44b2a3510b4cbb762eca673d929e0ae6c2828930
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-06-02 19:12:10 -0300

    Help no CORS

--- src/main/java/br/com/g_uni/api/ApiApplication.java
+++ src/main/java/br/com/g_uni/api/ApiApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
+
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "*", maxAge = 3600)
 public class ApiApplication {
 
 	public static void main(String[] args) {
