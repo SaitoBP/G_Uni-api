7abbaf29d4095d5ba21ada35937bffd85d53473f
Gabriel Saito <gabrielpereira050201@hotmail.com>
2020-06-29 19:30:42 -0300

    Adicionado documentação

--- src/main/java/br/com/g_uni/api/ApiApplication.java
+++ src/main/java/br/com/g_uni/api/ApiApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
-
+@EnableSwagger2
 public class ApiApplication {
 
 	public static void main(String[] args) {
